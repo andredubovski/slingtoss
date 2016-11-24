160bf35d48b3b39a3d694d68cb13158a9db8d323
Andre Dubovskiy <andrehchs@gmail.com>
2016-11-24 04:15:29 -0500

    blh

--- Rebound/UI/Tutoial.swift
+++ Rebound/UI/Tutoial.swift
@@ -12,16 +12,17 @@ class Tutorial: SKSpriteNode {
   
   var isShowing = false
   
-  let appear = SKAction.fadeAlpha(to: 1, duration: 0.4)
+  let appear = SKAction.fadeAlpha(to: 1, duration: 0.3)
   let pressIn = SKAction.scale(to: 0.95, duration: 0.04)
-  let releaseOut = SKAction.sequence([SKAction.scale(to: 1.1, duration: 0.05), SKAction.scale(to: 1, duration: 0.035)])
-  let fade = SKAction.fadeAlpha(to: 0, duration: 0.4)
+  let releaseOut = SKAction.sequence([SKAction.scale(to: 1.1, duration: 0.1)])
+  let fade = SKAction.fadeAlpha(to: 0, duration: 0.1)
   var isBeingTouched = false
   
   
   func show(scene: SKScene = gameScene) {
     scene.addChild(self)
     zPosition = 100
+    alpha = 0
     run(appear)
     isShowing = true
   }
